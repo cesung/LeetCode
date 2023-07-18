0ba01832eec5900f6a94b5dfaf88318bdbb93043
Cheng-En Sung <chengensung11235813@gmail.com>
2023-07-18 11:25:09 -0700

    316. Remove Duplicate Letters

---
+++ Categories/Monotonic_Stack/316_removeDuplicateLetters.py
@@ -0,0 +1,28 @@
+from typing import *
+from collections import defaultdict
+
+
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+        rcd = defaultdict(int)
+        for idx, ch in enumerate(s):
+            rcd[ch] = idx
+
+        vis = set()
+        stk = []
+        for idx, ch in enumerate(s):
+            if ch in vis:
+                continue
+
+            while (
+                stk and
+                ord(stk[-1]) > ord(ch) and
+                rcd[stk[-1]] > idx
+            ):
+                rm_ch = stk.pop()
+                vis.remove(rm_ch)
+            
+            stk.append(ch)
+            vis.add(ch)
+        
+        return "".join(stk)
