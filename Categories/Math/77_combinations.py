a18df4045b7405b553ec4fb11651719dcef6b9d1
Cheng-En Sung <chengensung11235813@gmail.com>
2023-08-01 09:52:32 -0700

    77. Combinations

---
+++ Categories/Math/77_combinations.py
@@ -0,0 +1,19 @@
+from typing import *
+
+class Solution:
+    def dfs(self, idx: int, cnt: int, comb: List[int]) -> None:
+        if cnt == self.k:
+            self.combs.append(comb[:])
+            return
+        
+        for jdx in range(idx, self.n + 1):
+            self.dfs(jdx + 1, cnt + 1, comb + [jdx])
+
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        self.n = n
+        self.k = k
+        self.combs = []
+        
+        self.dfs(1, 0, [])
+
+        return self.combs
